2d464f77415003583fdb31896d230e03d2705c02
VinodDevX <vinod@thought.com>
2025-10-31 17:06:35 +0530

    signup mobile key

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -10,7 +10,7 @@ export const signup = async (req, res) => {
     const userExists = await User.findOne({ email });
     if (userExists) return res.status(400).json({ message: 'User already exists' });
 
-    const user = await User.create({ name, email, password });
+    const user = await User.create({ name, email, password, contactNumber });
 
     res.status(201).json({
       _id: user._id,
